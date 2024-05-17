3e9499ab528ca210ec1c167f8ac1c4a6af4ec783
maniaghili <aligholimani@gmail.com>
2024-05-17 12:03:42 +0330

    add librari

--- login/login.js
+++ login/login.js
@@ -1,6 +1,6 @@
 let clic = document.querySelector('.btn_form')
 let hanoz = document.querySelector('.hanoz')
-
+let body = document.querySelector('body')
 clic.addEventListener('click',mani)
 
 function mani(e){
