e8b9f7688840d6012675070843892effd14b8087
maniaghili <aligholimani@gmail.com>
2024-05-20 00:12:28 +0330

    new code

--- login/login.js
+++ login/login.js
@@ -1,9 +1,26 @@
 let clic = document.querySelector('.btn_form')
-let hanoz = document.querySelector('.hanoz')
-let body = document.querySelector('body')
+var app = document.getElementById('app');
+
 clic.addEventListener('click',mani)
 
 function mani(e){
     e.preventDefault()
-    hanoz.style.display = 'inline'
+
+    var typewriter = new Typewriter(app, {
+        loop: true
+      });
+      
+      typewriter
+        .typeString('هنوز بک اند سایت تکمیل نشده هفته بعد که تکمیل شد بیا ثبت نام کن😉')
+        .pauseFor(300)
+        .deleteChars(70)
+        .typeString('حالا شاید هفته بعدم نشد 😂🤔')
+        .pauseFor(1000)
+        .deleteChars(40)
+        .start();
+    
 }
+
+
+
+
